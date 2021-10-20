9c10855e988abc8c5a7c9d811424fcfaff2bca3f
Duckyson <90754123+Duckyson@users.noreply.github.com>
2021-10-20 20:50:14 +0800

    tt

---
+++ 小宇.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define N 100
+int main(){
+	int a[N],i,m=0,n=0;
+	for(i=0;i<N;i+=5){
+		scanf("%d %d %d %d %d",&a[i],&a[i+1],&a[i+2],&a[i+3],&a[i+4]);
+		if(a[i]==0&&a[i+1]==0&&a[i+2]==0&&a[i+3]==0&&a[i+4]==0)/*输入的五个数不都为0*/ 
+		break;
+	}
+	for(i=0;i<N&&a[i]!=0||a[i+1]!=0||a[i+2]!=0||a[i+3]!=0||a[i+4];i+=5){
+		if((a[i+1]-a[i])==(a[i+2]-a[i+1])==(a[i+3]-a[i+2])==(a[i+4]-a[i+3])){
+			m=a[i+1]-a[i];
+			printf("case one\n%d %d %d %d %d\n",a[i+4]+m,a[i+4]+2*m,a[i+4]+3*m,a[i+4]+4*m,a[i+4]+5*m);
+		}
+		if(a[i]*a[i+2]==a[i+1]*a[i+1]&&a[i+1]*a[i+3]==a[i+2]*a[i+2]&&a[i+2]*a[i+4]==a[i+3]*a[i+3]){
+			n=a[i+1]/a[i];
+			printf("case two\n%d %d %d %d %d\n",a[i+4]*n,a[i+4]*n*n,a[i+4]*n*n*n,a[i+4]*n*n*n*n,a[i+4]*n*n*n*n*n);
+		}
+		if(a[i+2]==a[i]+a[i+1]&&a[i+3]==a[i+1]+a[i+2]&&a[i+4]==a[i+2]+a[i+3]){
+			printf("case three\n%d %d %d %d %d\n",a[i+3]+a[i+4],a[i+3]+2*a[i+4],2*a[i+3]+3*a[i+4],3*a[i+3]+5*a[i+4],5*a[i+3]+8*a[i+4]);
+		}
+	}
+	return 0;
+}
